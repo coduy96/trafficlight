1b5c5ea9c771c8bd292b25291047cd154f07afdf
Duy Co <versco@VersMac.local>
2020-02-08 16:42:34 +0700

    change some constructor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import React, { Component } from "react";
 import "./App.css";
 import TrafficLight from "./components/TrafficLight";
 
+const RED = 0;
+const ORANGE = 1;
+const GREEN = 2;
+
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = { curentColor: RED };
+    setInterval(
+      () =>
+        this.setState({
+          curentColor: this.getNextColor(this.state.curentColor)
+        }),
+      1000
+    );
+  }
+
+  getNextColor(color) {
+    switch (color) {
+      case RED:
+        return ORANGE;
+      case ORANGE:
+        return GREEN;
+      default:
+        return RED;
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <TrafficLight />
+        <TrafficLight curentColor = {this.state.curentColor}/>
       </div>
     );
   }
--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -7,31 +7,8 @@ const ORANGE = 1;
 const GREEN = 2;
 
 class TrafficLight extends Component {
-  constructor() {
-    super();
-    this.state = { curentColor: RED };
-    setInterval(
-      () =>
-        this.setState({
-          curentColor: this.getNextColor(this.state.curentColor)
-        }),
-      1000
-    );
-  }
-
-  getNextColor(color) {
-    switch (color) {
-      case RED:
-        return ORANGE;
-      case ORANGE:
-        return GREEN;
-      default:
-        return RED;
-    }
-  }
-
   render() {
-    const { curentColor } = this.state;
+    const { curentColor } = this.props;
     return (
       <div className="TrafficLight">
         <div
